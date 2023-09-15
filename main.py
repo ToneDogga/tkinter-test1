8115c74b74dd93f50a1c464bec7be6a34c385eb3
ToneDogga <41771145+ToneDogga@users.noreply.github.com>
2023-09-16 09:26:19 +0930

    test version to learn tkinter improved15

--- main.py
+++ main.py
@@ -12,61 +12,38 @@
 
 import tkinter as tk
 import subprocess
-import tempfile
+
 
 class CppCompilerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("C++ Compiler")
 
-        self.text_input = tk.Text(root, height=10, width=40)
+        self.text_input = tk.Text(self.root, height=10, width=40)
         self.text_input.pack()
-        self.text_input.bind("<KeyRelease-1>", self.compile_cpp)
+        self.text_input.bind("<KeyRelease>", self.compile_cpp)
 
-        self.output_text = tk.Text(root, height=10, width=40, state=tk.DISABLED)
+        self.output_text = tk.Text(self.root, height=10, width=40, state=tk.DISABLED)
         self.output_text.pack()
-
-        self.compiler_label = tk.Label(root, text="Select Compiler:")
-        self.compiler_label.pack()
-
-        self.compiler_var = tk.StringVar()
-        self.compiler_var.set("g++")  # Default to g++
-        self.compiler_option = tk.OptionMenu(root, self.compiler_var, "g++", "clang++")
-        self.compiler_option.pack()
-
-        self.optimization_label = tk.Label(root, text="Select Optimization Level:")
-        self.optimization_label.pack()
-
-        self.optimization_var = tk.StringVar()
-        self.optimization_var.set("-O2")  # Default to -O2
-        self.optimization_option = tk.OptionMenu(
-            root, self.optimization_var, "-O0", "-O1", "-O2", "-O3"
-        )
-        self.optimization_option.pack()
 
     def compile_cpp(self, event=None):
         code = self.text_input.get("1.0", tk.END)
         with open("temp.cpp", "w") as f:
             f.write(code)
 
-        compiler = self.compiler_var.get()
-        optimization = self.optimization_var.get()
+        try:
+            result = subprocess.check_output(["g++", "temp.cpp", "-o", "temp"])
+            output = result.decode("utf-8")
+        except subprocess.CalledProcessError as e:
+            output = e.output.decode("utf-8")
 
-        # Execute the compilation and assembly command using the shell
-        cmd = f"{compiler} -S temp.cpp -o temp {optimization} 2>&1"
-        result = subprocess.getoutput(cmd)
-
-        # Display the assembly code
         self.output_text.config(state=tk.NORMAL)
         self.output_text.delete("1.0", tk.END)
-        self.output_text.insert(tk.END, result)
+        self.output_text.insert(tk.END, output)
         self.output_text.config(state=tk.DISABLED)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = CppCompilerApp(root)
     root.mainloop()
-
-
-
-
